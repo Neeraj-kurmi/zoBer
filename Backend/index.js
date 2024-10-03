10380201bff3cc992f74c5cd518ce93fb445902f
Neeraj-kurmi <146584412+Neeraj-kurmi@users.noreply.github.com>
2024-10-03 22:50:43 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended :true}))
 app.use(cookieParser());
 const corsOptions ={
-    origin:"http://localhost:5173",
+    origin:"https://zober.onrender.com/",
     credentials:true
 }
 app.use(cors(corsOptions));
