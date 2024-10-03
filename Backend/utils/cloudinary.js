1aae8bd8f1eb3cdbbf97e73a4d123170135a647b
Neeraj-kurmi <146584412+Neeraj-kurmi@users.noreply.github.com>
2024-10-04 01:02:48 +0530

    Update cloudinary.js

--- Backend/utils/cloudinary.js
+++ Backend/utils/cloudinary.js
@@ -4,7 +4,7 @@ dotenv.config();
 cloudinary.config({ 
     cloud_name: process.env.CLOUD_NAME, 
     api_key: process.env.API_KEY, 
-    api_secret:process.env.SECRET_KEY // Click 'View API Keys' above to copy your API secret
+    api_secret:process.env.API_SECRET // Click 'View API Keys' above to copy your API secret
 });
 
 export default cloudinary
